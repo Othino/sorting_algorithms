871b62ff6acb45f91471795e19eda7e939f4a562
Othino <micahothino44@gmail.com>
2022-11-08 09:32:50 -0800

    Counting sort

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,52 +1,52 @@
 #include "sort.h"
-#include <stdlib.h>
-#include <stdio.h>
+#include "stdlib.h"
 
 /**
   * counting_sort - Afunction that sorts an array using counting algorithm.
   * @array: The array to sort.
   * @size: The length of the array.
-  * Return: Nothing.
+  * Return: void
   */
 void counting_sort(int *array, size_t size)
 {
-	unsigned int i = 1;
-	int *counter = NULL, k = 0, j = 0;
+	int i, max;
+	int *count = NULL, *copy = NULL;
+	size_t j, temp, total = 0;
+
 
 	if (array == NULL || size < 2)
 		return;
-
-	k = array[0];
-	for (; i < size; i++)
-	{
-		if (array[i] > k)
-			k = array[i];
-	}
-
-	counter = malloc(sizeof(int) * (k + 1));
-	if (counter == NULL)
+	copy = malloc(sizeof(int) * size);
+	if (copy == NULL)
 		return;
-
-	for (j = 0; j <= k; j++)
-		counter[j] = 0;
-	for (i = 0; i < size; i++)
-		counter[array[i]] += 1;
-	for (j = 0; j < k; j++)
+	for (j = 0, max = 0; j < size; j++)
 	{
-		counter[j + 1] += counter[j];
-		printf("%d, ", counter[j]);
+		copy[j] = array[j];
+		if (array[j] > max)
+			max = array[j];
 	}
-	counter[j + 1] += counter[j];
-	printf("%d\n", counter[j + 1]);
-	for (i = 0; i < size; i++)
+	count = malloc(sizeof(int) * (max + 1));
+	if (count == NULL)
 	{
-		j = counter[array[i]] - 1;
-		if (array[i] != array[j])
-		{
-			k = array[i];
-			array[i] = array[j];
-			array[j] = k;
-		}
+		free(copy);
+		return;
 	}
-	free(counter);
+	for (i = 0; i <= max; i++)
+		count[i] = 0;
+	for (j = 0; j < size; j++)
+		count[array[j]] += 1;
+	for (i = 0; i <= max; i++)
+	{
+		temp = count[i];
+		count[i] = total;
+		total += temp;
+	}
+	for (j = 0; j < size; j++)
+	{
+		array[count[copy[j]]] = copy[j];
+		count[copy[j]] += 1;
+	}
+	print_array(count, max + 1);
+	free(count);
+	free(copy);
 }
