dd5fa6c9cf12b599a4f2cf59694b9a462d29eb4c
Othino <micahothino44@gmail.com>
2022-11-08 09:26:35 -0800

    Shell sort

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -8,8 +8,8 @@
   */
 void shell_sort(int *array, size_t size)
 {
-	unsigned int i = 0, j = 0, gap = 0;
-	int aux = 0;
+	size_t i, j, gap = 1;
+	int insert;
 
 	if (array == NULL || size < 2)
 		return;
@@ -17,21 +17,16 @@ void shell_sort(int *array, size_t size)
 	while (gap < size / 3)
 		gap = gap * 3 + 1;
 
-	for (; gap > 0; gap = (gap - 1) / 3)
+	while (gap > 0)
 	{
 		for (i = gap; i < size; i++)
 		{
-			aux = array[i];
-			for (j = i; j >= gap && array[j - gap] > aux;
-					j -= gap)
-			{
-				if (array[j] != array[j - gap])
-					array[j] = array[j - gap];
-			}
-			if (array[j] != aux)
-				array[j] = aux;
-
+			insert = array[i];
+			for (j = i; j >= gap && array[j - gap] > insert; j = j - gap)
+				array[j] = array[j - gap];
+			array[j] = insert;
 		}
+		gap = (gap - 1) / 3;
 		print_array(array, size);
 	}
 }
