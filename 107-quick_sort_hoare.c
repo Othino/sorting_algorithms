53b4b1afdcf08d7ace4f1e041e6975c674e53bde
Othino <micahothino44@gmail.com>
2022-11-08 09:40:26 -0800

    Quick sort

--- 107-quick_sort_hoare.c
+++ 107-quick_sort_hoare.c
@@ -1,76 +1,83 @@
 #include "sort.h"
-#include <stdio.h>
-#include <stdlib.h>
-
+size_t qsh_partition(int *arr, size_t size, size_t left, size_t right);
+void swap_elem(int *arr, size_t size, int *a, int *b);
+void qsh_manager(int *arr, size_t size, size_t left, size_t right);
 /**
- * partition - finds the partition for the quicksort using the Hoare scheme
+ * quick_sort_hoare - quick sort, Hoare partition scheme
  * @array: array to sort
- * @lo: lowest index of the partition to sort
- * @hi: highest index of the partition to sort
- * @size: size of the array
- *
- * Return: index of the partition
- */
-size_t partition(int *array, ssize_t lo, ssize_t hi, size_t size)
-{
-	int swap, pivot;
-
-	pivot = array[hi];
-	while (lo <= hi)
-	{
-		while (array[lo] < pivot)
-			lo++;
-		while (array[hi] > pivot)
-			hi--;
-		if (lo <= hi)
-		{
-			if (lo != hi)
-			{
-				swap = array[lo];
-				array[lo] = array[hi];
-				array[hi] = swap;
-				print_array(array, size);
-			}
-			lo++;
-			hi--;
-		}
-	}
-	return (hi);
-}
-
-/**
- * quicksort - sorts a partition of an array of integers
- * @array: array to sort
- * @lo: lowest index of the partition to sort
- * @hi: highest index of the partition to sort
- * @size: size of the array
- *
- * Return: void
- */
-void quicksort(int *array, ssize_t lo, ssize_t hi, size_t size)
-{
-	ssize_t pivot;
-
-	if (lo < hi)
-	{
-		pivot = quicksort(array, lo, hi, size);
-		quicksort(array, lo, pivot, size);
-		quicksort(array, pivot + 1, hi, size);
-
-	}
-}
-
-/**
- * quick_sort_hoare - sorts an array of integers in ascending order using the
- * Quick sort algorithm
- * @array: The array to sort
- * @size: The size of the array
- *
- * Return: void
+ * @size: size of array
  */
 void quick_sort_hoare(int *array, size_t size)
 {
-	if (array == NULL || size < 2)
-		return;
-	quicksort(array, 0, size - 1, size);
+	if (array && size > 1)
+		qsh_manager(array, size, 0, size - 1);
+}
+/**
+ * qsh_manager - manage sort recursion
+ * @arr: original array
+ * @size: size of original array
+ * @left: leftmost element of partition
+ * @right: rightmost element of partition
+ */
+void qsh_manager(int *arr, size_t size, size_t left, size_t right)
+{
+	size_t p;
+
+	if (left < right)
+	{
+		p = qsh_partition(arr, size, left, right);
+
+		qsh_manager(arr, size, left, p - 1);
+		qsh_manager(arr, size, p, right);
+	}
+}
+/**
+ * qsh_partition - sorts partition according to pivot
+ * @arr: original array
+ * @size: size of original array
+ * @left: leftmost element of partition
+ * @right: rightmost element of partition
+ *
+ * Return: split point for new partitions
+ */
+size_t qsh_partition(int *arr, size_t size, size_t left, size_t right)
+{
+	int pivot = arr[right]; /* pivot is last elem */
+	size_t l = left - 1, r = right + 1;
+
+	while (1)
+	{
+		do {
+			l++;
+		} while (arr[l] < pivot);
+
+		do {
+			r--;
+		} while (arr[r] > pivot);
+
+		if (l >= r)
+			break;
+
+		swap_elem(arr, size, &arr[l], &arr[r]);
+	}
+	return (l);
+}
+/**
+ * swap_elem - swap value of array elements
+ * @arr: array (for print)
+ * @size: size of array (for print)
+ * @a: pointer to array element
+ * @b: pointer to array element
+ */
+void swap_elem(int *arr, size_t size, int *a, int *b)
+{
+	int tmp;
+
+	if (a != b)
+	{
+		tmp = *a;
+		*a = *b;
+		*b = tmp;
+		print_array(arr, size); /* print updated array */
+	}
 }
